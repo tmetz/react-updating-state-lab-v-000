8b0bfc0903b50c187f45c9b0cf6475f7ae4a0708
Tammy Metz <tammy@tammymetz.com>
2019-11-02 23:50:30 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,35 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react';
+
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  handleBRClick =() => {
+    // settings.bitrate to 12
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    });
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleBRClick}className="bitrate"></button>
+    )
+  }
+}
